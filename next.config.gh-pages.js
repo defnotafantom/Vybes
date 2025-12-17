6dc77941de68f3281d999853386bc73b4c1c2cdb
Dani <buccadany@gmail.com>
2025-12-17 19:08:49 +0100

    Fix: allow Vercel Blob images in next/image

--- next.config.gh-pages.js
+++ next.config.gh-pages.js
@@ -8,6 +8,11 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'i.imgur.com',
       },
+      // Vercel Blob public URLs (e.g. https://<id>.public.blob.vercel-storage.com/...)
+      {
+        protocol: 'https',
+        hostname: '**.public.blob.vercel-storage.com',
+      },
       {
         protocol: 'http',
         hostname: 'localhost',
